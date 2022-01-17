b7db00c83c7b7b800263a29a80b3d118bf17c395
WilliamKangas <william.a.kangas@gmail.com>
2022-01-17 15:04:47 +0100

    Added x and y ints to Vehicle along with their setters and getters

--- Vehicle.java
+++ Vehicle.java
@@ -11,6 +11,8 @@ public abstract class Vehicle {
     private double currentSpeed; // The current speed of the car
     private Color color;         // Color of the car
     private String modelName;    // The car model name
+    private int x;
+    private int y;
 
     public Vehicle(
         int nrDoors, 
@@ -21,6 +23,8 @@ public abstract class Vehicle {
             this.enginePower  = enginePower;
             this.color        = color;
             this.modelName    = modelName;
+            this.x            = 0;
+            this.y            = 0;
     }
 
     // region GETTERS n SETTERS
@@ -30,12 +34,21 @@ public abstract class Vehicle {
     public double getCurrentSpeed() { return currentSpeed; }
     public Color getColor()         { return color; }
     public String getModelName()    { return modelName; }
+    public int getX()               { return x; }
+    public int getY()               { return y; }
 
     public void setColor(Color clr){
 	    color = clr;
     }
 
-    // endof GETTERS n SETTERS
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+// endof GETTERS n SETTERS
 
     public void incrementSpeed(double amount){
 	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
