a25ecea66f4c95eed8e6f156183559766d51a9a6
WilliamKangas <william.a.kangas@gmail.com>
2022-01-17 14:35:32 +0100

    gas() and brake() first draft

--- Vehicle.java
+++ Vehicle.java
@@ -47,12 +47,16 @@ public abstract class Vehicle {
 
     // TODO fix this method according to lab pm
     public void gas(double amount){
-        incrementSpeed(amount);
+        if(amount >= 0 && amount <= 1){
+            incrementSpeed(amount);
+        }
     }
 
     // TODO fix this method according to lab pm
     public void brake(double amount){
-        decrementSpeed(amount);
+        if(amount >= 0 && amount <= 1){
+            decrementSpeed(amount);
+        }
     }
 
     public void startEngine(){
